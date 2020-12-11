005faae4a88e0f9607ffc179dac752fdd41f03ab
billwishop <willbishop.hello@gmail.com>
2020-12-11 15:40:52 -0600

    Delete buttons added on complete, incomplete and stems.

--- src/components/songs/CompleteSongs/CompleteSongCard.js
+++ src/components/songs/CompleteSongs/CompleteSongCard.js
@@ -1,32 +1,46 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom"
 import ReactAudioPlayer from 'react-audio-player';
+import { SongContext } from '../SongProvider'
 
 
-export const CompleteSongCard = ({ completeSong }) => (
-    <div className="audio__card">
+export const CompleteSongCard = ({ completeSong }) => {
+    const {deleteSong} = useContext(SongContext)
+    return (
+        
+        <div className="audio__card">
 
-        <h3 className="audio__title">
-            <Link to={`/song/${completeSong.id}`}>
-                {completeSong.songName}
-            </Link>
-        </h3>
+            <h3 className="audio__title">
+                <Link to={`/song/${completeSong.id}`}>
+                    {completeSong.songName}
+                </Link>
+            </h3>
 
-        <div className="audio__username">
-            <Link to={`/profile/${completeSong.userId}`}>
-                {completeSong.user.name}
-            </Link>
+            {completeSong.userId === parseInt(localStorage.getItem("app_user_id"))
+                ?   <div>
+                        <button onClick={evt =>{
+                        evt.preventDefault()
+                        deleteSong(completeSong.id)}
+                        }>delete</button>
+                    </div>
+                
+                :   <div className="audio__username">
+                        <Link to={`/profile/incomplete/${completeSong.userId}`}>
+                            {completeSong.user.name}
+                        </Link>
+                    </div>
+            }
+
+            <ReactAudioPlayer
+                key={completeSong.id}
+                className="audio__player"
+                src={completeSong.completeURL}
+                controls
+                />
+
         </div>
+    )
 
-        <ReactAudioPlayer
-            key={completeSong.id}
-            className="audio__player"
-            src={completeSong.completeURL}
-            controls
-            />
+}
 
-    </div>
-
-    
-)
 
--- src/components/songs/IncompleteSongs/IncompleteSongCard.js
+++ src/components/songs/IncompleteSongs/IncompleteSongCard.js
@@ -1,32 +1,43 @@
-import React from "react"
+import React, { useContext }  from "react"
 import { Link } from "react-router-dom"
 import ReactAudioPlayer from 'react-audio-player';
+import { SongContext } from '../SongProvider'
 
 
-export const IncompleteSongCard = ({ incompleteSong }) => (
-    <div className="audio__card">
+export const IncompleteSongCard = ({ incompleteSong }) => {
+    const {deleteSong} = useContext(SongContext)
+    return (
+        <div className="audio__card">
 
-        <h3 className="audio__title">
-            <Link to={`/song/${incompleteSong.id}`}>
-                {incompleteSong.songName}
-            </Link>
-        </h3>
+            <h3 className="audio__title">
+                <Link to={`/song/${incompleteSong.id}`}>
+                    {incompleteSong.songName}
+                </Link>
+            </h3>
 
-        <div className="audio__username">
-            <Link to={`/profile/incomplete/${incompleteSong.userId}`}>
-                {incompleteSong.user.name}
-            </Link>
+            {incompleteSong.userId === parseInt(localStorage.getItem("app_user_id"))
+                    ?   <div>
+                            <button onClick={evt =>{
+                                evt.preventDefault()
+                                deleteSong(incompleteSong.id)}
+                            }>delete</button>
+                        </div>
+                    
+                    :   <div className="audio__username">
+                            <Link to={`/profile/incomplete/${incompleteSong.userId}`}>
+                                {incompleteSong.user.name}
+                            </Link>
+                        </div>
+                }
+
+            <ReactAudioPlayer
+                key={incompleteSong.id}
+                className="audio__player"
+                src={incompleteSong.incompleteURL}
+                controls
+                />
+
         </div>
-
-        <ReactAudioPlayer
-            key={incompleteSong.id}
-            className="audio__player"
-            src={incompleteSong.incompleteURL}
-            controls
-            />
-
-    </div>
-
-    
-)
+    )
+}
 
