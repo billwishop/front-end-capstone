d84d722f413a94f6c236ebf8a43467b736c0a3d4
billwishop <willbishop.hello@gmail.com>
2020-12-15 10:33:57 -0600

    Added additional CSS to profile section and audio cards.

--- src/components/songs/IncompleteSongs/IncompleteBrowseList.js
+++ src/components/songs/IncompleteSongs/IncompleteBrowseList.js
@@ -19,24 +19,26 @@ export const IncompleteBrowseList = () => {
     }, [songs])
 
     return (
-        <div className="song__browse">
-            <div className="song__left">
-            {
-                filteredSongs.map(song => {
-                    return <IncompleteSongCard key={song.id} incompleteSong={song} />
-                })
-            }
-            </div>
-            <div className="song__right">
-                <h1>
-                    featurist - a collaborative musical environment
-                </h1>
-                <div>
-                    browse from and contribute to unfinished music projects from other users. 
-                    if you find a song you like, click on the title to see what other users have uploaded
-                    and contribute a stem to the song to be considered for the final version.
+        <div className="song__card">
+            <div className="song__browse">
+                <div className="song__left">
+                {
+                    filteredSongs.map(song => {
+                        return <IncompleteSongCard key={song.id} incompleteSong={song} />
+                    })
+                }
                 </div>
             </div>
+                <div className="song__right">
+                    <h1>
+                        featurist - a collaborative musical environment
+                    </h1>
+                    <div>
+                        browse from and contribute to unfinished music projects from other users. 
+                        if you find a song you like, click on the title to see what other users have uploaded
+                        and contribute a stem to the song to be considered for the final version.
+                    </div>
+                </div>
         </div>
     )
 }
