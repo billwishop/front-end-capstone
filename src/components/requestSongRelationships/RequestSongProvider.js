e6da59f39991cf7b940ed8a3a285011f241cefd7
billwishop <willbishop.hello@gmail.com>
2020-12-15 11:25:57 -0600

    Added two data providers and tables in my database. One table holds the request types so users can specify what they are looking for from other users when uploading an incomplete track. Also, users looking to contribute will be able to filter the browse list by requests. The second table added is a relationship table between songs and requests.

---
+++ src/components/requestSongRelationships/RequestSongProvider.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+export const RequestSongContext = React.createContext()
+
+export const RequestSongProvider = props => {
+    const [requestSongs, setRequestSongs] = useState([])
+
+    const getRequestSongs = () => {
+        return fetch("http://localhost:8088/requestsongrelationships/?_expand=request")
+            .then(res => res.json())
+            .then(setRequestSongs)
+    }
+
+    const addRequestSong = (relationshipObj) => {
+        return fetch("http://localhost:8088/requestsongrelationships", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(relationshipObj)
+        })
+            .then(getRequestSongs)
+    }
+
+    const deleteRequestSong = relationshipId => {
+        return fetch(`http://localhost:8088/requestsongrelationships/${relationshipId}`, {
+            method: "DELETE"
+        })
+            .then(getRequestSongs)
+    }
+
+    return (
+        <RequestSongContext.Provider value={
+            {
+                requestSongs, getRequestSongs, addRequestSong, deleteRequestSong
+            }
+        }>
+            {props.children}
+        </RequestSongContext.Provider>
+    )
+}
